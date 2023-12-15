fffbd6d5d16936d241f86a631ca0d3bcf8275736
Aliza Beverage <abeverage@berkeley.edu>
2023-12-15 09:12:11 -0800

    add post-processing to alfa.py

--- alfa/alfa.py
+++ alfa/alfa.py
@@ -5,7 +5,7 @@ import numpy as np
 import pandas as pd
 import csv
 import emcee
-# import corner
+import corner
 from multiprocessing import Pool
 import matplotlib.pyplot as plt
 #from schwimmbad import MPIPool
@@ -52,6 +52,7 @@ if __name__ == "__main__":
     nsteps = 1000
     nsteps_save = 300
     thin = 1
+    post_process = True
 
     # use command arguments to get filename
     if len(sys.argv)>1:
@@ -109,40 +110,66 @@ if __name__ == "__main__":
           )
     sampler.run_mcmc(pos, nsteps, progress=True);
 
-    
     # #~~~~~~~~~~~~~~~~~~~~~ post-process ~~~~~~~~~~~~~~~~~~~~~~~ #
-    
-    # samples = sampler.get_chain(flat=False, thin=thin,discard=nsteps-nsteps_save)
-    # np.save(f"{ALFA_OUT}{filename}_mcmc.npy",samples)
 
-    # #~~~~~~~~~~~~~~~~~~~~~ make plots ~~~~~~~~~~~~~~~~~~~~~~~ #
+    if post_process:
+        reader = emcee.backends.HDFBackend(f"{ALFA_OUT}{filename}.h5")
+        flat_samples = reader.get_chain(discard=nsteps-nsteps_save, thin=thin, flat=True)
 
-    # plot_outputs(data,grids,parameters_to_fit,filename,reader=sampler,thin=thin,discard=nsteps-nsteps_save)
 
-    # # correct abundances...
-    # flat_samples = sampler.get_chain(discard=nsteps-nsteps_save,flat=True, thin=thin)
-    # # make summary file with abundances corrected
-    # dict_results = {}
-    # for i,param in enumerate(parameters_to_fit):
-    #     dict_results[param+'16'] = [np.percentile(flat_samples[:,i],16)]
-    #     dict_results[param+'50'] = [np.median(flat_samples[:,i])]
-    #     dict_results[param+'84'] = [np.percentile(flat_samples[:,i],64)]
+        # corner plot
+        sel = np.ones(len(parameters_to_fit)).astype(bool)
+        fig = corner.corner(
+            flat_samples[:,sel], labels=np.array(parameters_to_fit)[sel],
+            show_titles=True
+        )
+        plt.savefig(f"{ALFA_OUT}{filename}_corner.jpeg")
     
-    #     if param in ['feh','ah','ch','nh','nah','mgh','sih',
-    #                           'kh','cah','tih','vh','crh','mnh','coh',
-    #                           'nih','cuh','srh','bah','euh']:
-    #         dist = correct_abundance(flat_samples[:,parameters_to_fit=='zH'].ravel(),
-    #                                  flat_samples[:,i],param)
-    #         param_st = '['+param[:-1].capitalize()+'/H]'
-    #         dict_results[param_st+'16'] = [np.percentile(dist,16)]
-    #         dict_results[param_st+'50'] = [np.median(dist)]
-    #         dict_results[param_st+'64'] = [np.percentile(dist,64)]
+        
+        # best-fit spectra
+        plt.figure(figsize=(10,3))
+        inds = np.random.randint(len(flat_samples), size=10)
+        for ind in inds:
+            sample = flat_samples[ind]
+            
+            params = get_properties(sample,parameters_to_fit)
+            mflux = g.get_model(params,outwave=data.wave)
+        
+            #poly norm
+            poly, mfluxnorm, data_r = polynorm(data, mflux,return_data=True)
+            plt.plot(data.wave,data_r, 'C0', lw=1)
+            plt.plot(data.wave,mfluxnorm, 'C1')
+            
+        
+        plt.xlabel('Wavelength')
+        plt.ylabel('Flux')
+    
+        plt.savefig(f"{ALFA_OUT}{filename}_bestspec.jpeg")
     
     
-    # df = pd.DataFrame.from_dict(dict_results)
-    # df.to_csv(f"{ALFA_OUT}{filename}.sum", 
-    #           float_format='%10.3f', sep=" ", 
-    #           quoting=csv.QUOTE_NONE, escapechar=" ")
+        # save outputs in summary file
+        parameters_to_fit = np.array(parameters_to_fit)
+        dict_results = {}
+        for i,param in enumerate(parameters_to_fit):
+            dict_results[param+'16'] = [np.percentile(flat_samples[:,i],16)]
+            dict_results[param+'50'] = [np.median(flat_samples[:,i])]
+            dict_results[param+'84'] = [np.percentile(flat_samples[:,i],64)]
+        
+            if param in ['feh','ah','ch','nh','nah','mgh','sih',
+                                  'kh','cah','tih','vh','crh','mnh','coh',
+                                  'nih','cuh','srh','bah','euh']:
+                dist = correct_abundance(flat_samples[:,parameters_to_fit=='zH'].ravel(),
+                                         flat_samples[:,i],param)
+                param_st = '['+param[:-1].capitalize()+'/H]'
+                dict_results[param_st+'16'] = [np.percentile(dist,16)]
+                dict_results[param_st+'50'] = [np.median(dist)]
+                dict_results[param_st+'64'] = [np.percentile(dist,64)]
+        
+        
+        df = pd.DataFrame.from_dict(dict_results)
+        df.to_csv(f"{ALFA_OUT}{filename}.sum", 
+                  float_format='%10.3f', sep=" ", 
+                  quoting=csv.QUOTE_NONE, escapechar=" ")
     
     
 
