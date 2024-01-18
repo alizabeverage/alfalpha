02b3eeb76e3d0183f9dd9380ffbb5b588a69bacb
Aliza Beverage <abeverage@berkeley.edu>
2024-01-17 17:33:18 -0800

    similar to alf

--- alfa/setup_params.py
+++ alfa/setup_params.py
@@ -9,7 +9,7 @@ def setup_params(parameters_to_fit=None):
                     'logemline_h':-4, 'logemline_ni':-4, 'logemline_nii':-4, 'logemline_oii':-4,
                    'logemline_oiii':-4, 'logemline_sii':-4, 'velz2':0, 'sigma2':200,'teff':0,
                      'jitter':1}
-    
+    '''
     priors_all = {'velz':[-200,200], 'sigma':[100,500], 'logage':[-0.1,1.2], 'zH':[-1.5,0.2],
                     'feh':[-0.3,0.3], 'ah':[-0.3,0.3], 'ch':[-0.15,0.15], 
                     'nh':[-0.3,0.3], 'nah':[-0.3,0.3], 'mgh':[-0.3,0.3], 'sih':[-0.3,0.3],
@@ -32,7 +32,7 @@ def setup_params(parameters_to_fit=None):
                     'logemline_nii':[-6,1], 'logemline_oii':[-6,1], 'logemline_oiii':[-6,1],
                     'logemline_sii':[-6,1], 'velz2':[-200,200], 'sigma2':[0,1000],'teff':[-50,50],
                     'jitter':[0.1,10]}
-    '''
+    
 
     if parameters_to_fit is None:
         return default_params, priors_all
@@ -48,8 +48,8 @@ def setup_initial_position(nwalkers,parameters_to_fit):
     pos = np.random.uniform(0,1,(nwalkers,len(parameters_to_fit)))
 
     # starting positions.... 
-    init_pos = {'velz':[-5,5], 'sigma':[200,300], 'logage':[0.4,1.0], 'zH':[-0.8,0.1],
-                    'feh':[-0.2,0.2], 'ah':[-0.2,0.2], 'ch':[-0.15,0.15], 
+    init_pos = {'velz':[-5,5], 'sigma':[200,300], 'logage':[0.1,0.4], 'zH':[-0.8,0.1],
+                    'feh':[-0.2,0.2], 'ah':[-0.2,0.2], 'ch':[-0.1,0.1], 
                     'nh':[-0.2,0.2], 'nah':[-0.2,0.2], 'mgh':[-0.2,0.2], 'sih':[-0.2,0.2],
                     'kh':[-0.2,0.2], 'cah':[-0.2,0.2], 'tih':[-0.2,0.2], 
                     'vh':[-0.2,0.2], 'crh':[-0.2,0.2], 'mnh':[-0.2,0.2], 'coh':[-0.2,0.2],
