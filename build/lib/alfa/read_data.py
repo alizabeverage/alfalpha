2d7477e88648d59247e19fec9b0e1ad2c53a9d90
Aliza Beverage <abeverage@berkeley.edu>
2024-03-23 10:44:31 -0700

    add build files

---
+++ build/lib/alfa/read_data.py
@@ -0,0 +1,46 @@
+import numpy as np
+import pandas as pd
+import os
+ALFA_HOME = os.environ['ALFA_HOME']
+
+class Data():
+    def __init__(self,filename=None,filename_exact=False):
+        self.filename = filename
+        self.filename_exact = filename_exact
+        if self.filename is None: 
+            return
+
+        if self.filename_exact:
+            tmp = np.array(pd.read_csv(f"{self.filename}.dat",
+                                       delim_whitespace=True, header=None, comment='#'))
+        else:
+            tmp = np.array(pd.read_csv(f"{ALFA_HOME}indata/{self.filename}.dat",
+                                       delim_whitespace=True, header=None, comment='#'))
+        self.wave = tmp[:,0]
+        self.flux = tmp[:,1]
+        self.err = tmp[:,2]
+        self.mask = tmp[:,3]
+        self.ires = tmp[:,4]
+
+        # this is how I'm dealing with masked values. Make nan
+        # then it's not included in the log prob
+        self.err[self.mask == 0] = np.nan
+
+        self.fitting_regions = []
+        self.get_fitting_regions()
+
+
+    def get_fitting_regions(self):
+        if self.filename_exact:
+            f = open(f"{self.filename}.dat",'r')
+        else:
+            f = open(f"{ALFA_HOME}indata/{self.filename}.dat",'r')
+        
+        lines = f.readlines()
+        f.close()
+        for line in lines[:50]:
+            if '#' in line: 
+                x1 = float(line.split()[1])
+                x2 = float(line.split()[2])
+                self.fitting_regions.append([x1,x2])
+        self.fitting_regions = 1e4*np.array(self.fitting_regions)
